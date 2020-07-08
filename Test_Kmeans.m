07cf8209140579af8d06d5b1e6c7f935a6bbe6b2
CerenPaja <63333528+CerenPaja@users.noreply.github.com>
2020-07-08 10:03:29 +0300

    Update Test_Kmeans.m

--- Test_Kmeans.m
+++ Test_Kmeans.m
@@ -5,18 +5,25 @@ val = double(variable);
  
 %% This creates new field in stats2 struct, fills it with double version of PixelValues
 
-for k = 1 : length(stats2);
+for k = 1 : length(stats2)
     stats2(k).Doubles = double(stats2(k).PixelValues);
 end
 %% This creates new field for std values
-for z = 1 : length(stats2);
+for z = 1 : length(stats2)
     stats2(z).Std_vals = std(stats2(z).Doubles);
 end
 
 %% Create dot ids
-for z = 1: length(stats2);
+for z = 1: length(stats2)
     stats2(z).Dotids = z;
 end
+
+%% Sum of pixels 
+
+for z = 1: length(stats2)
+    stats2(z).SumPixel = sum(stats2(z).Doubles);
+end
+
 
 %% TEST example with random data
 
@@ -33,14 +40,25 @@ TestMatrix(:,3)= (extractfield(stats2, 'MeanIntensity'))'; %3rd column
 
 TestMatrix(:,4)= kmeans(TestMatrix(:,2:3),6);
 
-%% Plot std vs mean intensity and color by cluster groups
-group = TestMatrix(:,4);
-gscatter(TestMatrix(:,3),TestMatrix(:,2),group);
+%% Custom made color map for plots
+cmp(1,:) = [0.8,0,0];% red
+cmp(2,:) = [0.9,0.6,0];%orange
+cmp(3,:) = [1,0.9,0];%yellow
+cmp(4,:) = [0.3,0.8,1];%cyan
+cmp(5,:) = [0,0.1,1];%dark blue
+cmp(6,:) = [0.8,0.2,1];%magenta
 
+%% Plot std vs mean intensity and color by cluster groups
+
+group = TestMatrix(:,4);
+gscatter(TestMatrix(:,3),TestMatrix(:,2),group,cmp);
+xlabel('Mean Intensity') 
+ylabel('Standart deviation Intensity')
 
 
 %% We force MeanIntensity values to be in a range between o to 1+eps
-% WE DONT USE THIS YET
+% WE DONT USE THIS
+% YET.................................................................
 
 [c,x] = ecdf(TestMatrix(:,3));                    % normalizing MeanIntensity Distribution
 most_vals = find(c>.99,1,'first');                 % take 99 percent of the distribution
@@ -49,6 +67,28 @@ mI = mI/x(most_vals)-eps/2;
 
 % save this mI result as the 5th column of our TestMatrix
 TestMatrix(:,5) = mI;
+
+%% Visualize the clusters
+% we need tmp_shift file for this (drag it into command window)
+
+%first create ind variable = selected cluster groups and their dot ids
+ind = TestMatrix((group > 2), :);
+ind = ind(:,1);
+
+%% Another way of choosing clusters
+ind = TestMatrix((group == 6), :);
+ind = ind(:,1);
+
+%% Create this variable for easy visualization
+DotCentre        = cat(1,stats2.Centroid);
+%% Visualization of the clusters
+figure
+imshow(max(tmp_shift,[],3),[])               
+hold on 
+plot(DotCentre(:,1),DotCentre(:,2),'b.')     
+plot(DotCentre(ind,1),DotCentre(ind,2),'r.')
+
+
 
 
 
